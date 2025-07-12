b452eaf8031973c296838415775b8be4b351b3a6
Soham2407 <nesarikarsoham24@gmail.com>
2025-07-12 12:29:30 +0530

    initial commit

---
+++ 01-Fundamentals-Part-1/02-variables.js
@@ -0,0 +1,19 @@
+let firstName = "Soham";
+console.log(firstName);
+
+// camelCase
+let myFavouriteLanguage = "Javascript";
+console.log(myFavouriteLanguage);
+
+// Variables name can start with letters, $, _
+let $year = 2025;
+let _courseName = "Javascript";
+
+// Varibles names must be descriptive
+let myFirstJob = "Programmer";
+let mySecondJob = "Coder";
+console.log(myFirstJob);
+
+// constants
+let PI = 3.14;
+console.log(PI);
---
+++ 01-Fundamentals-Part-1/assignment1.js
@@ -0,0 +1,5 @@
+let country = "India";
+let continent = "Asia";
+let population = 1.46;
+
+console.log(country, continent, population);
